a415ee0331c0c7270b77ed4942519d2fabb4a246
BryanJBarber <104166218+BryanJBarber@users.noreply.github.com>
2023-10-03 15:00:32 -0500

    updated db 10/3/23

---
+++ OneDrive/Desktop/js/ToTestTables.sql
@@ -0,0 +1,4 @@
+select * from auth_tokens;
+select * from user;
+select * from pic;
+select * from achievements;
--- OneDrive/Desktop/js/index.js
+++ OneDrive/Desktop/js/index.js
@@ -5,13 +5,15 @@
 const Sequelize = require('sequelize');
 const { DataTypes } = Sequelize;
 
-const sequelize = new Sequelize('logins', 'root', 'root', {             // logins is schema name and root is user and password for server
-    //host:
+const sequelize = new Sequelize('trailblazerdb', 'root', 'root',        //root is user and pass
+{             
+    //host: For google
     //port:
     dialect: 'mysql'
 });
 
-                                                                        //Proof of connection, Test using cmd: node index.js
+//Proof of connection, Also authenticates connection, Test using cmd: node index.js
+
 /*sequelize.authenticate().then(() => {
     console.log("Connection Successful");
 }).catch((err) => {
@@ -36,26 +38,139 @@ const Users = sequelize.define('user', {
         type: DataTypes.STRING,
         allowNull: false
     },
-    achieve1: {
-        type: DataTypes.BOOLEAN,
-        defaultValue: false
-    }
-                                                                        // More for any Achievements
 },
-
 {
     freezeTableName: true,                                              //takes away auto pluralization
     timestamps: false,                                                  // takes away timestamps in table
 
 });
 
+const Pictures = sequelize.define('pic', 
+{
+    id:
+    {
+      type: DataTypes.INTEGER,
+      primaryKey:true,
+      autoIncrement:true
+    },
+    user_id:{
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: 
+        {
+            model:Users,
+            key:'user_id'
+        }
+    },
+    image_URL: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    audio_URL: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    Name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+},
+{
+    freezeTableName: true,                                              
+    timestamps: false,                                                 
+
+});
+
+const Achievements = sequelize.define('Achievements', 
+{
+    id:
+    {
+      type: DataTypes.INTEGER,
+      primaryKey:true,
+      autoIncrement:true
+    },
+    user_id:{
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: 
+        {
+            model:Users,
+            key:'user_id'
+        }
+    },
+    Achieve1: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
+    },
+    Achieve2: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
+    },
+    Achieve3: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
+    }
+},
+{
+    freezeTableName: true,                                              
+    timestamps: false,                                                  
+
+});
+
+const moment = require('moment');
+const Auth = sequelize.define('auth_token',
+{
+    id:
+    {
+      type: DataTypes.INTEGER,
+      primaryKey:true,
+      autoIncrement:true
+    },
+    token:
+    {
+      type: DataTypes.STRING,
+      allowNull:false,
+      validate:
+      {
+        notEmpty:true
+      }
+    },
+    user_id:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references:
+      {
+        model:Users,
+        key:'user_id'
+      }
+    },
+    is_valid:
+    {
+      type: DataTypes.BOOLEAN,
+      defaultValue:true
+    },
+    issue_date:
+    {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW
+    },
+    expiration_date:
+    {
+      type: DataTypes.DATE,
+      defaultValue:moment().add(1,'days').toDate()
+    }
+  },{
+    timestamps:false
+});
+
 
 // DELETES CONTENTS FROM TABLE
-async function deleteAll() 
+async function deleteUsers() 
 {
     await Users.sync({alter: true}).then(() => 
     {
         return Users.destroy({truncate: true});
+        
     }).then((data) => 
     {
         console.log(data);
@@ -66,9 +181,53 @@ async function deleteAll()
     });
 }
 
+async function deletePic() 
+{
+    await Pictures.sync({alter: true}).then(() => 
+    {
+        return Pictures.destroy({truncate: true});
+    }).then((data) => 
+    {
+        console.log(data);
+    })
+    .catch((err) => 
+    {
+        console.log(err);
+    });
+}
 
-// single add
-async function add()
+async function deleteAchieve() 
+{
+    await Achievements.sync({alter: true}).then(() => 
+    {
+        return Achievements.destroy({truncate: true});
+    }).then((data) => 
+    {
+        console.log(data);
+    })
+    .catch((err) => 
+    {
+        console.log(err);
+    });
+}
+
+async function deleteAuth() 
+{
+    await Auth.sync({alter: true}).then(() => 
+    {
+        return Auth.destroy({truncate: true});
+    }).then((data) => 
+    {
+        console.log(data);
+    })
+    .catch((err) => 
+    {
+        console.log(err);
+    });
+}
+
+// ADD TO TABLE FOR TEST
+async function adduser()
 {
     await Users.sync({alter: true}).then((data) => {                            
     return Users.create({
@@ -81,42 +240,69 @@ async function add()
 })
 }
 
-
-// bulk add
-async function bulkAdd()
+async function addpic()
 {
-    Users.sync({alter: true}).then((data) => {                             
-    return Users.bulkCreate([{
-        username: "Bryan",
-        password: "Poop",
-        achieve1: true
-    },
-    {
-        username: "Lauren",
-        password: "Shit",
-        achieve1: true
-    },
-    {
-        username: "Falynne",
-        password: "Pee",
-        achieve1: true
-    },
-    {
-        username: "Ashley",
-        password: "Piss",
-        achieve1: true
-    }
-]);
-}).then((data) => {
-    console.log("Users added to database")
-    data.forEach(element => {
-        console.log(element.toJSON())                                  
+    await Pictures.sync({alter: true}).then((data) => {                            
+    return Pictures.create({
+        user_id: 1,
+        image_URL: "imagetest",
+        audio_URL: "audiotest",
+        Name: "Pic1"
     });
-}).catch((err) => {
-    console.log(err)
-});
+}).then((data) => {
+    console.log(data.toJSON());
+})
 }
 
-//bulkAdd();
-//add();
-//deleteAll();
+async function addachieve()
+{
+    await Achievements.sync({alter: true}).then((data) => {                            
+    return Achievements.create({
+        user_id: 1,
+        achieve1: true,
+        Achieve2: false,
+        Achieve3: true
+    });
+}).then((data) => {
+    console.log(data.toJSON());
+})
+}
+
+async function addauth()
+{
+    await Auth.sync({alter: true}).then((data) => {                            
+    return Auth.create({
+        user_id: 1,
+        token: "Tokentest",
+        
+    });
+}).then((data) => {
+    console.log(data.toJSON());
+})
+}
+
+//CALL THESE ONE AT A TIME TO TEST
+
+//addauth();
+//addachieve();
+//addpic();
+//adduser();
+
+//CALL THESE ONE AT A TIME TO DELETE CONTENTS IN EACH TABLE
+
+//deletePic();
+//deleteAchieve();
+//deleteAuth();
+
+//UNCOMMENT AND CALL THESE ONE BY ONE TO DELETE USER TABLE CONTENTS TO NOT GET FOREIGN KEY ERROR
+
+//sequelize.query('SET GLOBAL FOREIGN_KEY_CHECKS = 0;', { raw: true });
+//deleteUsers();
+//sequelize.query('SET GLOBAL FOREIGN_KEY_CHECKS = 1;', { raw: true });
+
+
+
+module.exports = 
+{
+  Users, Pictures, Achievements, Auth
+}
